77ca733754bab08a5d790bfd4d3a3e663daa5e65
Savannah C <sgcorrell@gmail.com>
2019-05-24 16:45:48 +0000

    Automatically backed up by Learn

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -113,13 +113,13 @@ describe Student do
   describe '.find_by' do 
     it 'executes the SQL to find a row by the attribute passed into the method' do 
       Student.new({name: "Susan", grade: 10}).save
-      expect(Student.find_by({name: "Susan"})).to eq([{"id"=>1, "name"=>"Susan", "grade"=>10, 0=>1, 1=>"Susan", 2=>10}])
+      expect(Student.find_by({name: "Susan"})).to eq([{"id"=>1, "name"=>"Susan", "grade"=>10}]) #0=>1, 1=>"Susan", 2=>10
     end
 
     it 'accounts for when an attribute value is an integer' do
       Student.new({name: "Susan", grade: 10}).save
       Student.new({name: "Geraldine", grade: 9}).save
-      expect(Student.find_by({grade: 10})).to eq([{"id"=>1, "name"=>"Susan", "grade"=>10, 0=>1, 1=>"Susan", 2=>10}])
+      expect(Student.find_by({grade: 10})).to eq([{"id"=>1, "name"=>"Susan", "grade"=>10}]) #0=>1, 1=>"Susan", 2=>10
     end
   end
 end
