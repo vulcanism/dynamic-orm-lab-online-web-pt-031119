396f6b0def63615ef8b72e97a07406356724ca21
Savannah C <sgcorrell@gmail.com>
2019-05-24 16:47:18 +0000

    Done.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -93,7 +93,7 @@ describe Student do
     describe '#save' do 
       it 'saves the student to the db' do 
         new_student.save
-        expect(DB[:conn].execute("SELECT * FROM students WHERE name = 'Sam'")).to eq([{"id"=>1, "name"=>"Sam", "grade"=>11, 0=>1, 1=>"Sam", 2=>11}])
+        expect(DB[:conn].execute("SELECT * FROM students WHERE name = 'Sam'")).to eq([{"id"=>1, "name"=>"Sam", "grade"=>11}]) #0=>1, 1=>"Sam", 2=>11
       end
 
       it 'sets the student\'s id' do
@@ -106,20 +106,20 @@ describe Student do
   describe '.find_by_name' do 
     it 'executes the SQL to find a row by name' do 
       Student.new({name: "Jan", grade: 10}).save
-      expect(Student.find_by_name("Jan")).to eq([{"id"=>1, "name"=>"Jan", "grade"=>10, 0=>1, 1=>"Jan", 2=>10}])
+      expect(Student.find_by_name("Jan")).to eq([{"id"=>1, "name"=>"Jan", "grade"=>10}]) #0=>1, 1=>"Jan", 2=>10
     end
   end
 
   describe '.find_by' do 
     it 'executes the SQL to find a row by the attribute passed into the method' do 
       Student.new({name: "Susan", grade: 10}).save
-      expect(Student.find_by({name: "Susan"})).to eq([{"id"=>1, "name"=>"Susan", "grade"=>10, 0=>1, 1=>"Susan", 2=>10}])
+      expect(Student.find_by({name: "Susan"})).to eq([{"id"=>1, "name"=>"Susan", "grade"=>10}]) #0=>1, 1=>"Susan", 2=>10
     end
 
     it 'accounts for when an attribute value is an integer' do
       Student.new({name: "Susan", grade: 10}).save
       Student.new({name: "Geraldine", grade: 9}).save
-      expect(Student.find_by({grade: 10})).to eq([{"id"=>1, "name"=>"Susan", "grade"=>10, 0=>1, 1=>"Susan", 2=>10}])
+      expect(Student.find_by({grade: 10})).to eq([{"id"=>1, "name"=>"Susan", "grade"=>10}]) #0=>1, 1=>"Susan", 2=>10
     end
   end
 end
