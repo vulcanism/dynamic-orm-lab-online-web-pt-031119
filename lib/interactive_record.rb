4073da7e193597633a2d1cb8a1b38f479b3c8a51
Savannah C <sgcorrell@gmail.com>
2019-05-24 16:38:26 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,4 +53,9 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
   
+  def self.find_by(hash)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0].to_s} = '#{hash.values[0].to_s}'"
+    DB[:conn].execute(sql)
+  end
+  
 end
